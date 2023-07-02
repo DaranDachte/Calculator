5ecd37d950e1fc3275015c415cd2c95b9b33465e
Daran Dachte <darandachte@gmail.com>
2023-07-02 15:38:33 +0200

    logic for tipButton and inputNumber was created

--- Calculator/src/components/InputNumber/InputNumber.tsx
+++ Calculator/src/components/InputNumber/InputNumber.tsx
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { FormEvent } from "react";
 
+import "./InputNumber.style.css";
 type InputNumberType = {
   value: number;
-  label: string;
+  title: string;
   icon: React.ReactNode;
+  OnSubmit: (value: number) => void;
 };
 
 export const InputNumber: React.FunctionComponent<InputNumberType> = ({
   value,
-  label,
+  title,
   icon,
+  OnSubmit,
 }) => {
-  return <div>InputNumber</div>;
+  const SubmitHandler = (e) => {
+    OnSubmit(Number(e.target?.value));
+  };
+
+  return (
+    <label className="inputNumber">
+      <span>{title}</span>
+      <div className="inputNumber_wrapper">
+        {icon}
+        <input type="number" value={value} onInput={SubmitHandler} />
+      </div>
+    </label>
+  );
 };
--- Calculator/src/components/TipButton/TipButton.tsx
+++ Calculator/src/components/TipButton/TipButton.tsx
@@ -1,5 +1,24 @@
 import React from "react";
+import "./TipButton.style.css";
 
-export const TipButton = () => {
-  return <div>TipButton</div>;
+type TipButtonType = {
+  value: number;
+  onClick: (value: number) => void;
+  active: boolean;
+};
+
+export const TipButton: React.FunctionComponent<TipButtonType> = ({
+  value,
+  onClick,
+  active,
+}) => {
+  const classNames = ["tipButton"];
+  if (active) {
+    classNames.push("tipButton_active");
+  }
+  return (
+    <button className={classNames.join(" ")} onClick={() => onClick(value)}>
+      {value} %
+    </button>
+  );
 };
